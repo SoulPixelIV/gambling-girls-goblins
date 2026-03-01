7e31feababd3260055709c13bef8b533bff14bee
SoulPixelIV <william.dj@outlook.de>
2026-03-01 17:52:17 +0100

    Player & Enemy can now draw Cards independently

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -12,10 +12,12 @@ extends Node
 var player_health = 100
 var turn_state = -1 #Turnstate -1 -> First Enemy Draw | Turnstate 0 -> Normal Enemy Draw | Turnstate 1 -> Player Draw
 var delay_timer = 2
-var enemy_card_index = 0
 var card_index = 0
+var enemy_card_index = 0
 var player_score = 0
 var enemy_score = 0
+var player_out = false
+var enemy_out = false
 var deck = ["2H", "2D", "2C", "2S", "3H", "3D", "3C", "3S", "4H", "4D", "4C", "4S", 
 "5H", "5D", "5C", "5S", "6H", "6D", "6C", "6S", "7H", "7D", "7C", "7S", 
 "8H", "8D", "8C", "8S", "9H", "9D", "9C", "9S", "10H", "10D", "10C", "10S", 
@@ -33,7 +35,7 @@ func _process(delta: float) -> void:
 		turn_state = 1
 	
 	#Enemys Turn
-	if turn_state == 0:
+	if turn_state == 0 && !enemy_out:
 		delay_timer -= delta
 		if delay_timer < 0:
 			spawn_enemy_playing_card(260 + 100 * enemy_card_index, 160)
@@ -41,12 +43,26 @@ func _process(delta: float) -> void:
 			turn_state = 1
 			delay_timer = 2
 			
+	#Enemy keeps going when Player is out
+	if player_out:
+		player_score_text.add_theme_color_override("font_color", Color(1, 0, 0))
+		turn_state = 0
+		
+	#Player keeps going when Enemy is out
+	if enemy_out:
+		enemy_score_text.add_theme_color_override("font_color", Color(1, 0, 0))
+		turn_state = 1
+			
 func _on_hit_button_pressed() -> void:
 	#Draw Card
-	if turn_state == 1:
+	if turn_state == 1 && !player_out:
 		spawn_playing_card(110 + 100 * card_index, 500)
 		card_index += 1
-		game_manager.turn_state = 0
+		turn_state = 0
+		
+func _on_stand_button_pressed() -> void:
+	if card_index > 0:
+		player_out = true
 		
 func spawn_playing_card(x, y):
 	if deck.size() > 0:
@@ -73,6 +89,14 @@ func _on_card_played(value):
 	player_score += value
 	player_score_text.text = str(player_score)
 	
+	#Check if Player is over 21
+	if player_score > 21:
+		player_out = true
+	
 func _on_card_played_enemy(value):
 	enemy_score += value
 	enemy_score_text.text = str(enemy_score)
+	
+	#Check if Enemy is over 21
+	if enemy_score > 21:
+		enemy_out = true
