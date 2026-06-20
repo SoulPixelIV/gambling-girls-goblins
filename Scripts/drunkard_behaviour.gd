3e504c14d08daf3cf400e59a891782a31c9c1380
SoulPixelIV <william.dj@outlook.de>
2026-06-21 01:06:23 +0200

    Small tweaks

--- Scripts/drunkard_behaviour.gd
+++ Scripts/drunkard_behaviour.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var health = 26
-var stand_on = 16
+var stand_on = 14
 var stand_chance = 0.3
 var deck = ["6H", "6D", "6C", "6S", "7H", "7D", "7C", "7S", "6H", "6D", "6C", "6S", "7H", "7D", "7C", "7S"]
 
