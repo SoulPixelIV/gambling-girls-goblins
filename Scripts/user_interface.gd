8e1ed702e8c8cf451d457324f537e69923c9b861
SoulPixelIV <william.dj@outlook.de>
2026-09-04 19:16:58 +0200

    Enemy damage is now shown in UI

--- Scripts/user_interface.gd
+++ Scripts/user_interface.gd
@@ -5,15 +5,17 @@ extends Node
 @onready var damage_player_label: RichTextLabel = $"Damage_Player_Label"
 @onready var totalDamage_player_label: RichTextLabel = $"TotalDamage_Player_Label"
 @onready var multiplier_player_label: RichTextLabel = $"Multiplier_Player_Label"
+@onready var totalDamage_enemy_label: RichTextLabel = $"TotalDamage_Enemy_Label"
 
 var curr_player_damage = 0
+var curr_enemy_damage = 0
 
 func _process(delta: float) -> void:
 	#Multiplier Label
 	var multiplier = game_manager.calculate_player_damage_multiplier()
 	multiplier_player_label.text = "x" + str(multiplier)
 	
-	#Damage Label
+	#Player Damage Label
 	if game_manager.enemy_score < 22:
 		curr_player_damage = max(game_manager.player_score - game_manager.enemy_score, 0)
 			
@@ -34,3 +36,13 @@ func _process(delta: float) -> void:
 		damage_player_label.text = str(game_manager.player_score)
 		totalDamage_player_label.text = "[color=orange]" + str(int(round(game_manager.player_score * multiplier))) + "[/color]"
 	
+	#Enemy Damage Label
+	if game_manager.player_score < 22:
+		curr_enemy_damage = max(
+			game_manager.enemy_score - game_manager.player_score,
+			0
+		)
+		totalDamage_enemy_label.text = str(curr_enemy_damage)
+	else:
+		curr_enemy_damage = game_manager.enemy_score
+		totalDamage_enemy_label.text = "[color=orange]" + str(curr_enemy_damage) + "[/color]"
