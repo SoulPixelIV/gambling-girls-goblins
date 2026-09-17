f3fe062eed62de6f49caf5768c27d5eb1a8f0a3a
SoulPixelIV <william.dj@outlook.de>
2026-09-17 18:43:30 +0200

    Reseting a run now resets highscore

--- Scripts/death_manager.gd
+++ Scripts/death_manager.gd
@@ -5,4 +5,10 @@ func _on_restart_button_pressed() -> void:
 	Global.player_funny_stat = 0
 	Global.player_unlucky_stat = 0
 	Global.player_lucky_stat = 0
+	
+	Global.layer_count = 1
+	Global.enemy_count = 0
+	Global.damage_count = 0
+	Global.score_count = 0
+	
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
